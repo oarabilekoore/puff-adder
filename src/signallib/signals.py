9f25c41cc76f6598170371f818f6f78f6c7caf0e
Oarabile Koore <oarabilekoore@protonmail.com>
2024-12-28 00:48:34 +0200

    added computed to siganls

--- src/signallib/signals.py
+++ src/signallib/signals.py
@@ -1,23 +1,41 @@
-# Signal Implementation In Python.
+class signal:    
+    def __init__(self, default=None):
+        self.__subscriptions = set()
+        self.__computedDeps = set()
+        self.__value = default
 
-class signal: 
-    def __init__(self, default_value = None):
-        self._internal_variable = default_value
-        self._subscribers = []
-
-    def _notify(self):
-        for subscriber in self._subscribers:
-            subscriber(self._internal_variable)
+    def __notify(self):
+        for subscriber in self.__subscriptions:
+            subscriber(self.value)
+        
+        for dep in self.__computedDeps:
+            dep.recompute()  
     
-    def subscribe(self, Fn):
-        self._subscribers.append(Fn)
+    def subscribe(self, fn):
+        self.__subscriptions.add(fn)
+        return lambda: self.__subscriptions.remove(fn)    
     
     @property
     def value(self):
-        return self._internal_variable
+        return self.__value
     
     @value.setter
     def value(self, new_value):
-        self._internal_variable = new_value
-        self._notify()
-
+        if new_value != self.__value:
+            self.__value = new_value
+            self.__notify()
+    
+    def computed(self, compute_fn):
+        computed_signal = signal(compute_fn(self.__value))
+        
+        # Define recompute method specifically for this computed signal
+        def recompute():
+            new_value = compute_fn(self.__value)
+            if new_value != computed_signal.value:
+                computed_signal.value = new_value
+        
+        computed_signal.recompute = recompute    
+        
+        # Add to dependencies
+        self.__computedDeps.add(computed_signal)
+        return computed_signal
